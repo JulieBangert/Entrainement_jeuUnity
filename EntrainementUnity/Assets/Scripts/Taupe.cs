59046536a341232bb6fb5af876c2123949db1644
FishJu <72547601+FishJu@users.noreply.github.com>
2020-10-09 19:43:06 +0200

    mon update - Julie
    Update - Julie
    
    Co-Authored-By: Leo <56823876+Litomy@users.noreply.github.com>

---
+++ EntrainementUnity/Assets/Scripts/Taupe.cs
@@ -0,0 +1,59 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Runtime.InteropServices;
+using TMPro.Examples;
+using UnityEngine;
+
+public class Taupe : MonoBehaviour
+{
+
+    public bool isTaupeCache = false;
+
+    public int positionActuelle = 0;
+
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        if (isTaupeCache)
+        {
+            print("Taupe caché !!"); 
+        }
+        else
+        {
+            Destroy(gameObject);
+            print("La taupe est KO");
+            ScoreScript.AugmenterLeScore();
+        }
+
+    }
+
+    void Start()
+    {
+        
+        StartCoroutine(MyCoroutine());
+
+    }
+
+
+    IEnumerator MyCoroutine()
+    {
+        print("MyCoroutine commence son exécution.");
+
+        Destroy(gameObject, 3.0f);
+        if(isTaupeCache == false)
+        {
+            GameManager.Instance.gestionTaupePerdu();
+        }
+ 
+        yield return new WaitForSeconds(4);
+        print("MyCoroutine a terminée son exécution.");
+        
+        
+    }
+  
+        
+}
+
+
