c6720d820eff7a432ca1e9e4ee9aa2612af4a5a6
Litomy <leo.db40@gmail.com>
2020-10-09 19:48:21 +0200

    Revert "Revert "mon update - Julie""
    This reverts commit 91885151fb1865e9c05f9049a92d3053a4f334a9.

---
+++ EntrainementUnity/Assets/Scripts/TaupeSpawnerScript.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Net;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TaupeSpawnerScript : MonoBehaviour
+{
+    public GameObject[] taupe;
+    public Transform[] spawnPoint;
+
+    private int rand;
+    private int randPosition;
+    
+    public float startTimeBtwSpawns;
+    private float timeBtwSpawns;
+    
+
+    private void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+   
+
+    private void Update()
+    {
+
+        if (GameManager.Instance.isJeuEnCours)
+        {
+
+            Taupe[] taupes = GameObject.FindObjectsOfType<Taupe>();
+
+            if(taupes.Length == spawnPoint.Length)
+            {
+                Debug.Log("PLUS DE PLACE !!");
+                return;
+            }
+
+            if (timeBtwSpawns <= 0)
+            {
+                rand = UnityEngine.Random.Range(0, taupe.Length);
+
+                bool isPositionLibre = false;
+
+                while(isPositionLibre == false)
+                {
+
+                    Debug.Log("Pas de position libre !");
+
+                    randPosition = UnityEngine.Random.Range(0, spawnPoint.Length);
+
+                    //Tu boucles sur toutes les taupes pour vérifier si randPosition est déjà existant dans une des taupes
+
+                    
+
+                    isPositionLibre = true;
+
+                    foreach (Taupe taupe in taupes)
+                    {
+                        if(taupe.positionActuelle == randPosition)
+                        {
+                            isPositionLibre = false;
+                        }
+                    }
+                }
+
+
+
+
+
+
+                GameObject Taupe = Instantiate(taupe[rand], spawnPoint[randPosition].transform.position, Quaternion.identity);
+                Taupe.GetComponent<Taupe>().positionActuelle = randPosition;
+                timeBtwSpawns = startTimeBtwSpawns;
+            }
+            else
+            {
+                timeBtwSpawns -= Time.deltaTime;
+            }
+        }
+
+           
+         
+
+        
+        
+    }
+    
+}
